570a597f11056a3be4aaf2e39e045c0bcd4afe80
Abhijeet-FSD <TathagatAbhijeet@gmail.com>
2025-01-05 00:51:21 +0530

    Dot Net APIS to fetch the product

---
+++ Backend/Dot Net Web Apis/Admin_Web_APIs/Program.cs
@@ -0,0 +1,32 @@
+using Admin_Web_APIs.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+
+builder.Services.AddSingleton<MongoDbService>(); //Adding MongoDBservice here for DI
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
