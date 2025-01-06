02806b7772a3a77499ad7ecb22b17da22cd9490e
Aashish Raj <aashish10raj@gmail.com>
2025-01-06 18:12:18 +0530

    implemented rotes and the login page basic UI

--- Frontend/vendingui/src/App.js
+++ Frontend/vendingui/src/App.js
@@ -3,16 +3,24 @@ import "./main.css";
 import HomePage from "./Components/VendingMachineHomePage/homePage";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import VendingMachineHomePage from "./Components/VendingMachineHomePage/homePage";
+import LoginPage from "./Components/LoginPage/login";
 
 const App = () => {
 
 
   return (
+    <Router>
     <div className="app">
       <Header />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        </Routes>
       <Footer />
     </div>
+    </Router>
   );
 };
 
---
+++ Frontend/vendingui/src/Components/LoginPage/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginPage = () => {
+
+return(
+    <div className="login-container">
+        <h1>LOGIN PAGE</h1>
+    </div>
+);
+
+};
+
+export default LoginPage;
