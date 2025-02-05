05fe10183d21266d8126c7f276ffc4ac3feaa3c9
Aashish Raj <aashish10raj@gmail.com>
2025-02-05 19:05:38 +0530

    fixed non-unique constraint error while updating the balance

--- Backend/users/src/main/java/com/vendingmachine/usermanagement/services/BalanceService.java
+++ Backend/users/src/main/java/com/vendingmachine/usermanagement/services/BalanceService.java
@@ -3,6 +3,11 @@ package com.vendingmachine.usermanagement.services;
 import com.vendingmachine.usermanagement.model.Balance;
 import com.vendingmachine.usermanagement.repository.BalanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,20 +18,21 @@ public class BalanceService {
     @Autowired
     private BalanceRepository balanceRepository;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     public Optional<Balance> getBalanceByUserId(int userId) {
         return balanceRepository.findByUserId(userId);
     }
 
     public Balance createOrUpdateBalance(int userId, double amount) {
-        Optional<Balance> existingBalance = balanceRepository.findByUserId(userId);
-        if (existingBalance.isPresent()) {
-            Balance balance = existingBalance.get();
-            balance.setBalance(amount);
-            return balanceRepository.save(balance);
-        } else {
-            Balance newBalance = new Balance(userId, amount);
-            return balanceRepository.save(newBalance);
-        }
+        Query query = new Query(Criteria.where("userId").is(userId));
+        Update update = new Update().set("balance", amount);
+
+        // Perform atomic upsert operation
+        return mongoTemplate.findAndModify(query, update,
+                org.springframework.data.mongodb.core.FindAndModifyOptions.options().returnNew(true).upsert(true),
+                Balance.class);
     }
 
 }
